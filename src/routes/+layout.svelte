f53c4c5e81a42ab696040f07fb01d9e8f4532209
Brian Beard <brian.a.beard@gmail.com>
2024-09-30 18:06:26 -0500

    fix deploy script

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,6 +3,9 @@
 	import type { Snippet } from 'svelte';
 	import { ModeWatcher } from 'mode-watcher';
 
+	// Render static site for gh-pages
+	export const prerender = true;
+
 	type Props = {
 		children: Snippet;
 	};
